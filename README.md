8b2f4ff659b0c3d4c5811b2fb89e9c1e32a5d412
heycalmdown <heyacct@gmail.com>
2016-07-22 16:49:21 +0900

    add) 기본 구현

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # watcha_star
-왓차 별점 가져오머
+
+더 왓차 별점 가져오머
+
+
+## 기본 동작
+
+- 유저 별 `watcha` 정보를 긁어와서 `cards.json`으로 백업을 해놓는다
+- 영화 타이틀과 별점만 뽑아서 출력한다
+
+
+## Options
+
+- user: `watcha` 유저 키
+- output
+  - console
+  - csv
+- update: `cards.json`이 있어도 강제로 긁어옴
---
+++ index.js
@@ -0,0 +1,79 @@
+const superagent = require('superagent-bluebird-promise');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const request = superagent.agent();
+const fs = Promise.promisifyAll(require('fs'));
+
+const CARDS = 'cards.json'
+
+// stolen from http://stackoverflow.com/questions/29375100/while-loop-using-bluebird-promises/29396005#29396005
+function promiseWhile(predicate, action) {
+  function loop(result) {
+    if (!predicate(result)) return;
+    return Promise.resolve(action(result)).then(loop);
+  }
+  return Promise.resolve().then(loop);
+}
+
+let cards = [];
+
+function getPage(username, page) {
+  return request.get(`https://watcha.net/v2/users/${username}/movies.json?filter%5Bsorting%5D=my_rating&page=${page}`)
+    .then(res => {
+      console.log(res.body.next_page);
+      cards = cards.concat(res.body.cards);
+      return res;
+    });
+}
+
+function readStars() {
+  return fs.readFileAsync(CARDS, 'utf-8')
+    .then(JSON.parse).catch(err=> console.error(err));
+}
+
+function gatherStars(username) {
+  if (!username) throw new Error('no username');
+  const predicate = (res) => !res || !!res.body.next_page;
+  const action = (res) => getPage(username, _.get(res, 'body.next_page', 1))
+  return promiseWhile(predicate, action)
+    .then(() => fs.writeFileAsync(CARDS, JSON.stringify(cards)).then(cards))
+}
+
+
+const argv = require('minimist')(process.argv.slice(2));
+const state = {
+  forceUpdate: !!argv.update,
+  user: process.env.WATCHA_USER || argv.user,
+  output: argv.output || 'console'
+};
+
+function checkUptodate() {
+  if (state.forceUpdate) return Promise.reject(new Error('out of date'));
+  return fs.accessAsync(CARDS); 
+}
+
+function output(cards) {
+  const handlers = {
+    console: cards => console.log(cards),
+    csv: cards => {
+      const header = Object.keys(cards[0]);
+      const contents = _.map(cards, card => `"${card.title}",${card.rating}`);
+      return fs.writeFileAsync('cards.csv', [header].concat(contents).join('\n'));
+    }
+  }
+  return (handlers[state.output] || handlers.console)(cards);
+}
+
+function main() {
+  checkUptodate()
+    .catch((err) => gatherStars(state.user))
+    .then(() => readStars())
+    .then((cards) => {
+      const picked = _.map(cards, card => ({title: card.items[0].item.title, rating: card.items[0].item.owner_action.rating}))
+      return picked;
+    })
+    .then(output)
+    .catch(err => console.error(err));
+}
+
+main();
